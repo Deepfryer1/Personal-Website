68fd53c7688b6ada201630f708d5eca9498c26dd
Secure Information <securepd@Michealla-s-S20-FE.localdomain>
2024-09-23 16:20:35 -0400

    newest changes

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 console.log(`Hello World`)
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  console.log(window.location.origin)
+  window.location.href = window.location.origin + '/success.html'
+})
